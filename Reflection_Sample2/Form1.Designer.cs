b1b8a360d35dfe5054626ae10e8aad4fdc5d130f
sudip-sharma <sudip.20@hotmail.com>
2017-05-18 21:05:55 +0530

    Sample 2 for Reflection

---
+++ Reflection_Sample2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Reflection_Sample2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTypeName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBoxMethods = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxProperties = new System.Windows.Forms.ListBox();
+            this.listBoxConstructors = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Type Name";
+            // 
+            // txtTypeName
+            // 
+            this.txtTypeName.Location = new System.Drawing.Point(151, 56);
+            this.txtTypeName.Name = "txtTypeName";
+            this.txtTypeName.Size = new System.Drawing.Size(461, 26);
+            this.txtTypeName.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(646, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Discover Type Information";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBoxMethods
+            // 
+            this.listBoxMethods.FormattingEnabled = true;
+            this.listBoxMethods.ItemHeight = 20;
+            this.listBoxMethods.Location = new System.Drawing.Point(35, 148);
+            this.listBoxMethods.Name = "listBoxMethods";
+            this.listBoxMethods.Size = new System.Drawing.Size(263, 504);
+            this.listBoxMethods.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Methods";
+            // 
+            // listBoxProperties
+            // 
+            this.listBoxProperties.FormattingEnabled = true;
+            this.listBoxProperties.ItemHeight = 20;
+            this.listBoxProperties.Location = new System.Drawing.Point(381, 148);
+            this.listBoxProperties.Name = "listBoxProperties";
+            this.listBoxProperties.Size = new System.Drawing.Size(261, 504);
+            this.listBoxProperties.TabIndex = 5;
+            // 
+            // listBoxConstructors
+            // 
+            this.listBoxConstructors.FormattingEnabled = true;
+            this.listBoxConstructors.ItemHeight = 20;
+            this.listBoxConstructors.Location = new System.Drawing.Point(700, 148);
+            this.listBoxConstructors.Name = "listBoxConstructors";
+            this.listBoxConstructors.Size = new System.Drawing.Size(261, 504);
+            this.listBoxConstructors.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(377, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Properties";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(696, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Constructors";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1086, 693);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxConstructors);
+            this.Controls.Add(this.listBoxProperties);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxMethods);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTypeName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTypeName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBoxMethods;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxProperties;
+        private System.Windows.Forms.ListBox listBoxConstructors;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Reflection_Sample2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reflection_Sample2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string typeName = txtTypeName.Text;
+            Type T = Type.GetType(typeName);
+            listBoxMethods.Items.Clear();
+            listBoxProperties.Items.Clear();
+            listBoxConstructors.Items.Clear();
+
+            MethodInfo[] objMethods =  T.GetMethods();
+
+            foreach (MethodInfo methodInfo in objMethods)
+            {
+                listBoxMethods.Items.Add(methodInfo.ReturnType + " " + methodInfo.Name);
+            }
+
+            PropertyInfo[] objProperties = T.GetProperties();
+
+            foreach (PropertyInfo propertyInfo in objProperties)
+            { 
+                listBoxProperties.Items.Add(propertyInfo.PropertyType.Name + " " + propertyInfo.Name);
+            }
+
+            ConstructorInfo[] objConstructors = T.GetConstructors();
+
+            
+            foreach (ConstructorInfo constructorInfo in objConstructors)
+            { 
+                listBoxConstructors.Items.Add(constructorInfo.ToString());
+            }
+        }
+    }
+}
